7eb76e8f2a1d9448d794da281bfb209d740ebc40
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-13 10:23:50 +0000

    update solution

--- index.js
+++ index.js
@@ -11,8 +11,24 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
+let squares = Array(nums.length).fill(0);
+    let left = 0,
+    right = nums.length - 1,
+    highest = nums.length - 1
+    while(left <= right) {
+        let leftSq = nums[left]**2;
+        let rightSq = nums[right]**2;
+        if(leftSq>rightSq){
+            squares[highest] = leftSq;
+            left++;
+        } else {
+            squares[highest] = rightSq;
+            right--;
+        }
+        highest--
+    }
+    return squares
 
 
 }
 
-nums = [-4,-1,0,3,10]
